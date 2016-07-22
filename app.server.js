5df32d16ce660912396ed75d708cfdd7b34343cc
Valentyn Yakymenko <vyakimenko@codeminders.com>
2016-07-22 13:12:21 +0300

    * server update. * package dependencies update. * remove appveyor.

--- app.server.js
+++ app.server.js
@@ -1,44 +1,4 @@
-var
-  express = require('express'),
-  path = require('path'),
-  compression = require('compression');
-
-var port = 9000,
-  app = express();
-
-app.use(compression());
-
 /**
- * Api Routes.
+ * @ng2 Server Runner.
  */
-
-
-
-/**
- * Static.
- */
-app.use('/js', express.static(path.resolve(__dirname, 'dist/prod/js')));
-app.use('/css', express.static(path.resolve(__dirname, 'dist/prod/css')));
-app.use('/assets', express.static(path.resolve(__dirname, 'dist/prod/assets')));
-
-/**
- * Spa Res Sender.
- * @param req {any}
- * @param res {any}
- */
-var renderIndex = function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'dist/prod/index.html'));
-};
-
-/**
- * Prevent server routing and use @ng2-router.
- */
-app.get('/*', renderIndex);
-
-/**
- * Server with gzip compression.
- */
-var server = app.listen(port, function() {
-  var port = server.address().port;
-  console.log('App is listening on port:' + port);
-});
+require('./server')();
---
+++ server/index.js
@@ -0,0 +1,51 @@
+var
+  express = require('express'),
+  path = require('path'),
+  compression = require('compression');
+
+var port = 9000,
+    _proddir = '../dist/prod',
+    app = express();
+
+var ServerInit = function () {
+
+  app.use(compression());
+
+  /**
+   * Api Routes.
+   */
+
+
+
+  /**
+   * Static.
+   */
+  app.use('/js', express.static(path.resolve(__dirname, _proddir+'/js')));
+  app.use('/css', express.static(path.resolve(__dirname, _proddir+'/css')));
+  app.use('/assets', express.static(path.resolve(__dirname, _proddir+'/assets')));
+
+  /**
+   * Spa Res Sender.
+   * @param req {any}
+   * @param res {any}
+   */
+  var renderIndex = function(req, res) {
+    res.sendFile(path.resolve(__dirname, _proddir+'/index.html'));
+  };
+
+  /**
+   * Prevent server routing and use @ng2-router.
+   */
+  app.get('/*', renderIndex);
+
+  /**
+   * Server with gzip compression.
+   */
+  var server = app.listen(port, function() {
+    var port = server.address().port;
+    console.log('App is listening on port:' + port);
+  });
+
+};
+
+module.exports = ServerInit;

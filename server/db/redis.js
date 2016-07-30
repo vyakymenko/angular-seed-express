81518cce08c0d8bcc565d67f68bb2a81d74ae70e
Valentyn Yakymenko <vyakimenko@codeminders.com>
2016-07-30 08:07:01 +0300

    * pre-config

---
+++ server/db/redis.js
@@ -0,0 +1,11 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
+client.sadd("names-list:name",
+    "Edsger Dijkstra",
+    "Donald Knuth",
+    "Alan Turing",
+    "Grace Hopper",
+  redis.print);
+
+client.quit();

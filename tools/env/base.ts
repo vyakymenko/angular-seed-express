b992b4fa6f816ef2cbd055e332c1441affec0fc5
Valentyn Yakymenko <vyakimenko@codeminders.com>
2016-08-22 11:03:25 +0300

    * update per angular2-seed parent

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -1,5 +1,9 @@
-export = {
-  ENV: 'BASE(DEV)',
+import {EnvConfig} from './env-config.interface';
+
+const BaseConfig: EnvConfig = {
+  // API url
   API: 'http://localhost:9001'
 };
+
+export = BaseConfig;
 
--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -1,5 +1,9 @@
-export = {
+import {EnvConfig} from './env-config.interface';
+
+const ProdConfig: EnvConfig = {
   ENV: 'PROD',
   API: ''
 };
+
+export = ProdConfig;
 

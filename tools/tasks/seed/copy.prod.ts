e2672fd20edf22cff214439bb05c9668ced093f2
Valentyn Yakymenko <vyakimenko@codeminders.com>
2016-09-07 23:11:12 +0300

    * clien prod build upd.

--- tools/tasks/seed/copy.prod.ts
+++ tools/tasks/seed/copy.prod.ts
@@ -1,19 +1,19 @@
 import * as gulp from 'gulp';
 import { join } from 'path';
 
-import { APP_SRC, TMP_DIR } from '../../config';
+import { APP_CLIENT_SRC, TMP_CLIENT_DIR } from '../../config';
 
 /**
  * Executes the build task, copying all TypeScript files over to the `dist/tmp` directory.
  */
 export = () => {
   return gulp.src([
-      join(APP_SRC, '**/*.ts'),
-      join(APP_SRC, '**/*.html'),
-      join(APP_SRC, '**/*.css'),
-      join(APP_SRC, '**/*.json'),
-      '!' + join(APP_SRC, '**/*.spec.ts'),
-      '!' + join(APP_SRC, '**/*.e2e-spec.ts')
+      join(APP_CLIENT_SRC, '**/*.ts'),
+      join(APP_CLIENT_SRC, '**/*.html'),
+      join(APP_CLIENT_SRC, '**/*.css'),
+      join(APP_CLIENT_SRC, '**/*.json'),
+      '!' + join(APP_CLIENT_SRC, '**/*.spec.ts'),
+      '!' + join(APP_CLIENT_SRC, '**/*.e2e-spec.ts')
     ])
-    .pipe(gulp.dest(TMP_DIR));
+    .pipe(gulp.dest(TMP_CLIENT_DIR));
 };

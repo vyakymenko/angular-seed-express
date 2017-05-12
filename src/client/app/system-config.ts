d39b159253a6b053116047271817743eec979e8a
Valentyn Yakymenko <vyakimenko@codeminders.com>
2017-05-12 10:56:06 +0300

    * reset, update in progress

--- src/client/app/system-config.ts
+++ src/client/app/system-config.ts
@@ -1,3 +1,8 @@
-declare var System: SystemJSLoader.System;
+/**
+ * Since package update:
+ *
+ * "@types/systemjs": "^0.20.2",
+ */
+// declare var System: SystemJSLoader.System;
 
 System.config(JSON.parse('<%= SYSTEM_CONFIG_DEV %>'));

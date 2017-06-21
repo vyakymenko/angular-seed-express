431e50c1783dabf8a26e077f674c7e4a9d250331
Valentyn Yakymenko <vyakimenko@codeminders.com>
2017-06-21 10:16:49 +0300

    * additional update and optimization.

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -366,7 +366,7 @@ export class SeedConfig {
     { src: 'intl/dist/Intl.min.js', inject: 'shims' },
     { src: 'systemjs/dist/system.src.js', inject: 'shims', buildType: BUILD_TYPES.DEVELOPMENT },
     // Temporary fix. See https://github.com/angular/angular/issues/9359
-    { src: '.tmp/Rx.min.js', inject: 'libs', buildType: BUILD_TYPES.DEVELOPMENT },
+    { src: 'rxjs/Rx.js', inject: 'libs', buildType: BUILD_TYPES.DEVELOPMENT }
   ];
 
   /**
